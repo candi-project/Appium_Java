c7448f5fb12ef4c87df2b3d2089f812e2ab91151
Candi615 <cchiu615@gmail.com>
2021-07-13 18:18:04 +0200

    first commit

---
+++ src/basics.java
@@ -0,0 +1,20 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class basics extends base{
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		AndroidDriver<AndroidElement> driver = capabilities();
+		
+		/*
+		 * xpath syntax:   //Tagname[@attribute='value']
+		 */
+		
+		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+
+	}
+
+}
